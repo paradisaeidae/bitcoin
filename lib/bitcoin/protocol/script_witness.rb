aec5932fb277af22958c6e18f5f217f6ead389ae
MarkT <paradisaeidae@gmail.com>
2021-09-17 14:29:22 +1000

    Remove some segwit references.

--- lib/bitcoin/protocol/script_witness.rb
+++ lib/bitcoin/protocol/script_witness.rb
@@ -1,15 +1,14 @@
 module Bitcoin
 module Protocol
 # TxWitness section of https://en.bitcoin.it/wiki/Protocol_documentation#tx
-class ScriptWitness
-# witness stack
+class ScriptWitness # witness stack
 attr_reader :stack
 def initialize
  @stack = [] end
-# check empty
-def empty?
+
+def empty? # check empty
  stack.empty? end
-# output script in raw binary format
-def to_payload
+
+def to_payload # output script in raw binary format
  payload = Bitcoin::Protocol.pack_var_int(stack.size)
  payload << stack.map { |e| Bitcoin::Protocol.pack_var_int(e.bytesize) << e }.join end end end end
